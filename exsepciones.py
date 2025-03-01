ba2bc9fdfa3e393888f6ecace45f69448579aa22
Fatimon1 <gonzalez.fatima.2024@cbtis-246.edu.mx>
2025-02-28 20:06:10 -0600

    Add files via upload

---
+++ exsepciones.py
@@ -0,0 +1,81 @@
+#fatima guadalupe gonzalez garcia
+#excepsiones
+#28/ febrero / 2025
+#2-j
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hola"
+
+
+# In[5]:
+
+
+if'cesar' == cesar.nombre:
+    print("eres cesar")
+
+
+# In[9]:
+
+
+x = 10 / 0
+
+
+# In[19]:
+
+
+lista = {1,2,3}
+print(lista[4])
+
+
+# In[21]:
+
+
+lista = [1,2,3]
+print(lista[4])
+
+
+# In[25]:
+
+
+edad = 20
+if adad > 18:
+print("la edad es superior a 18")
+    print("puede conducir")
+si o no
+print("la edad es inferior a 18")
+
+
+# In[27]:
+
+
+num = 1445
+cap = num.capitalize()
+print(cap)
+
+
+# In[29]:
+
+
+diccionario = {"nombre": "Juan", "edad": 25}
+print(diccionario["dirección"])
+
+
+# In[41]:
+
+
+x = int("abc")
+
+
+# In[45]:
+
+
+def factorial(n):
+    return n * factorial(n - 1)
+
+print(factorial(5))
+
